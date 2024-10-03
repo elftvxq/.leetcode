637dddfb745fab6996f48ee4a38927c271cb5a48
Emma Shih <emma.shih@shopee.com>
2024-10-04 01:18:05 +0800

    first commit

---
+++ 1.two-sum.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=1 lang=javascript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  // 当前算法已经是最优解决方案，时间复杂度为O(n)
+  // 我们可以稍微优化一下代码的可读性和性能
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    map.set(nums[i], i);
+  }
+// @lc code=end
